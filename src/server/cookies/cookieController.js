bd51204bae0280db2944497d64bc9d5916251573
Jason Ou <jasonou@Jasons-MacBook-Pro.local>
2018-11-05 12:12:07 -0500

    changes to setting cookies

--- src/server/cookies/cookieController.js
+++ src/server/cookies/cookieController.js
@@ -1,19 +1,31 @@
 const cartController = require('../cart/cartController')
+const Cart = require('../cart/cartModel.js');
 
 const cookieController = {};
 cookieController.setCookie = setCookie;
 cookieController.resetCookie = resetCookie;
 
 function setCookie(req, res, next) {
-  // console.log(req.cookies);
+  //if no cookie exists...
   if(!req.cookies['jasonandfriends-cart']){
     const rand = Math.floor(Math.random() * 1000000);
     res.cookie('jasonandfriends-cart', rand, {expire : new Date() + 9999});
 
     cartController.createCart(rand);
+    next();
+  } 
+  //confirm that the cookie exists in the db
+  else {
+    Cart.find({
+      cookieId : req.cookies['jasonandfriends-cart']
+    })
+    .then(response => {
+      if(response.length === 0) {
+        cartController.createCart(req.cookies['jasonandfriends-cart']);
+      }
+      next();
+    });
   }
-  
-  next();
 }
 
 function resetCookie(req, res, next) {
